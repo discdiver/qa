6125aa75639baa112c6ac56e47c787ed117c64da
Jeff Hale <jeffmshale@gmail.com>
2023-10-04 15:12:43 -0400

    update storage

--- remote_gh_secure.py
+++ remote_gh_secure.py
@@ -1,10 +1,11 @@
 from prefect import serve
 from prefect.deployments import remote_flow_to_deployment
 from prefect.blocks.system import Secret
+from prefect.runner.storage import GitRepository
 
 if __name__ == "__main__":
     serve(
-        remote_flow_to_deployment(
+        storage=GitRepository(
             url="https://github.com/discdiver/qa.git",
             access_token=Secret.load("gh-token").get(),
             entrypoint="basic_flow.py:my_flow",
