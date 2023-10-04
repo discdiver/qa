9168481efcba8a1f9fac353764fe33d6d80f63b2
Jeff Hale <jeffmshale@gmail.com>
2023-10-04 15:08:00 -0400

    add private repo serve

---
+++ remote_gh_secure.py
@@ -0,0 +1,13 @@
+from prefect import serve
+from prefect.deployments import remote_flow_to_deployment
+from prefect.blocks.system import Secret
+
+if __name__ == "__main__":
+    serve(
+        remote_flow_to_deployment(
+            url="https://github.com/discdiver/qa.git",
+            access_token=Secret.load("gh-token").get()
+            entrypoint="basic_flow.py:my_flow",
+            name="test-remote1",
+        )
+    )
