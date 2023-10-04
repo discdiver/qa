87e9b8a24d236a8098e7633835f314d5495afefe
Jeff Hale <jeffmshale@gmail.com>
2023-10-04 15:13:12 -0400

    update storage

--- remote_gh_secure.py
+++ remote_gh_secure.py
@@ -6,7 +6,7 @@ from prefect.runner.storage import GitRepository
 if __name__ == "__main__":
     serve(
         storage=GitRepository(
-            url="https://github.com/discdiver/qa.git",
+            repository="https://github.com/discdiver/qa.git",
             access_token=Secret.load("gh-token").get(),
             entrypoint="basic_flow.py:my_flow",
             name="test-remote1",
